ecc52a521bedf2a8f1efb771908e55fc668a213a
BearFox <nick.mjac@gmail.com>
2017-11-16 18:04:16 +1300

    change design of header and menu to match shop

--- src/Menu/components/List/index.js
+++ src/Menu/components/List/index.js
@@ -1,23 +1,15 @@
-import React, { Component } from "react";
-import clickOutside from "react-click-outside";
+import React from "react";
 
 import styles from "./list.css";
 
-class List extends Component {
-  handleClickOutside = () => {
-    this.props.toggle();
-  }
-  render() {
-    return (
-      <div className={styles.menu}>
-        {
-          this.props.buttons.map(
-            b => <a key={Math.random()} href={b.link} className={styles.item}>{b.label}</a>
-          )
-        }
-      </div>
-    );
-  }
-}
+const List = ({ buttons }) => (
+  <div className={styles.container}>
+    {
+      buttons.map(
+        b => <a key={b.label} href={b.link} className={styles.item}>{b.label}</a>
+      )
+    }
+  </div>
+);
 
-export default clickOutside(List);
+export default List;
--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,37 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 
 import List from "./components/List";
 import styles from "./menu.css";
 
 const buttons = [
-  { label: "sign up", link: "https://www.childershop.co.nz/en/signup" },
-  { label: "shop", link: "http://www.childershop.co.nz" },
-  { label: "depot service", link: "https://www.childershop.co.nz/en/infos/how_to_use" },
-  { label: "about us", link: "https://www.childershop.co.nz/en/infos/about" },
+  { label: "About", link: "https://www.childershop.co.nz/en/infos/about" },
+  { label: "Depot Service", link: "https://www.childershop.co.nz/en/infos/how_to_use" },
+  { label: "Shop", link: "http://www.childershop.co.nz" },
 ]
 
-class Menu extends Component {
-  state = {
-    showMenu: false,
-  }
-  toggleMenu = () => {
-    this.setState({ showMenu: !this.state.showMenu })
-  }
-  render() {
-    const { showMenu } = this.state;
-    return (
-      <div className={styles.container}>
-        <a href="https://www.childershop.co.nz/en/login" className={styles.login}>login</a>
-        <div className={styles.hamcontainer} onClick={this.toggleMenu} >
-        {
-          showMenu ?
-            <List buttons={buttons} toggle={this.toggleMenu} /> :
-            <div className={styles.hamburger} />
-        }
-        </div>
-      </div>
-    )
-  }
-}
+const Menu = ({ showMenu }) =>  (
+  <div className={styles.container}>
+    <List buttons={buttons} />
+    <a href="https://www.childershop.co.nz/en/signup" className={styles.login}>Sign up</a>
+    <a href="https://www.childershop.co.nz/en/login" className={styles.login}>Log in</a>
+  </div>
+)
 
 export default Menu;
